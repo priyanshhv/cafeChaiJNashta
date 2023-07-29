30a17538589a60d1944e91f64401cda2c0e915f5
PRIYANSH <20ce02009@iitbbs.ac.in>
2023-07-29 07:23:33 +0530

    again fixed cors error

--- index.js
+++ index.js
@@ -18,9 +18,17 @@ mongoose.connect(process.env.MONGO_URL)
 //Middlewares
 app.use(express.json())
 
-app.use(cors({
-    origin: " https://cafechainashta.onrender.com/"
-}))
+// app.use(cors({
+//     origin: " https://cafechainashta.onrender.com/"
+// }))
+
+ app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
 //Routes
 app.use("/api/auth",authRoute);
