9ef0de733b581baca8bfe33f87fa5c1b407f7c71
PRIYANSH <20ce02009@iitbbs.ac.in>
2024-09-15 12:50:52 +0530

    changed cors policy

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json())
  app.use(function (req, res, next) {
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Methods", " GET, POST, PUT, DELETE, OPTIONS");
     // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     res.header("Access-Control-Allow-Headers", "*");
     res.setHeader('Access-Control-Allow-Credentials', false);
